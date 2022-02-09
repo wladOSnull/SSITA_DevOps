f5791509d3962df295891a65e1f8d4bcc7658c67
wlados <wlad1324@gmail.com>
2022-02-09 21:47:50 +0200

    DC2-3 added 3, 4, 5

---
+++ python/hw4/hw4_deb.py
@@ -0,0 +1,23 @@
+import sys, deb_pkg_tools.package
+
+### opening & inspecting .deb file 
+##################################################
+package = deb_pkg_tools.package.inspect_package_fields(sys.argv[1])
+
+### printing specified fields from header
+##################################################
+print(package['Package'],
+        package['Version'],
+        sep='\n')
+
+### printing all fields from header
+##################################################
+for element in package:
+    print(element, '...........', package[element])
+
+### usage
+##################################################
+'''
+Executing this script:
+~ python3 hw4_deb.py
+'''
