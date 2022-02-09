5360978c7ec4f431725af763ae3c6b49fc72b228
wlados <wlad1324@gmail.com>
2022-02-10 01:08:58 +0200

    DC2-40 files: first hw diagram

--- DaaC/test/test.py
+++ DaaC/test/test.py
@@ -3,5 +3,12 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
+from diagrams.onprem.database import Postgresql
+import diagrams.onprem.database as dt
+
 with Diagram("Web Service", show=False):
     ELB("lb") >> EC2("web") >> RDS("userdb")
+    Postgresql("kek") >> dt.Scylla("kek2")
+    Postgresql("kek1")
+    Postgresql("kek2")
+    dt.Cassandra("casa")
